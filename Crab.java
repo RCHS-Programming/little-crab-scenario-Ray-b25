10a957d42f3f0d156ffc8a39d49626e223053bb3
Ray-b25 <raymond.brandeberry@stu.boone.kyschools.us>
2021-03-15 08:36:29 -0400

    Added worms and random crab movement

--- Crab.java
+++ Crab.java
@@ -9,10 +9,20 @@ public class Crab extends Actor
     {
         if (isAtEdge ())
             {
-        turn(17);    
-            
+        turn(17);   
     }
+    
+    if(Greenfoot.getRandomNumber(100)<10)
+        {
+            turn(Greenfoot.getRandomNumber(91)-45);
+        }
+    
     move();
+    
+    if(isTouching(Worm.class))
+    {
+        removeTouching(Worm.class);
+    }
 }
     public void move()
     {
