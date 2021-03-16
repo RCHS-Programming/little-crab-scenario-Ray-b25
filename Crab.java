6db39b1f9cf319ca7345357c6da2845e882a88e5
Ray-b25 <raymond.brandeberry@stu.boone.kyschools.us>
2021-03-16 08:40:20 -0400

    Organized code and added lobsters

--- Crab.java
+++ Crab.java
@@ -7,26 +7,50 @@ public class Crab extends Actor
 {
     public void act()
     {
-        if (isAtEdge ())
-            {
-        turn(17);   
+        turnAtEdge();
+        move();
+        eatWorm();
+        randomTurn();
     }
-    
-    if(Greenfoot.getRandomNumber(100)<10)
+    /*
+     * Turns the crab 17 degrees to the right if it is
+     * touching the edge.
+     */
+    public void turnAtEdge()
+    {
+        if (isAtEdge ())
+        {
+            turn(17);   
+        }   
+    }
+    /*
+     * Moves the crab 5 constantly.
+     */
+    public void move()
+    {
+         move(5);
+    }
+    /*
+     *Check whether crab is touching a worm. If it is, eat the worm.
+     *If not, do nothing. 
+     */
+    public void eatWorm()
+    {
+        if(isTouching(Worm.class))
+        {
+            removeTouching(Worm.class);
+        }
+    }
+    /*
+     * Turns the crab randomly left or right up to 45 degrees 10% of 
+     * the time.
+     */
+    public void randomTurn()
+    {
+        if(Greenfoot.getRandomNumber(100)<10)
         {
             turn(Greenfoot.getRandomNumber(91)-45);
         }
-    
-    move();
-    
-    if(isTouching(Worm.class))
-    {
-        removeTouching(Worm.class);
     }
-}
-    public void move()
-    {
-     move(5);
-}
 }
 
---
+++ Lobster.java
@@ -0,0 +1,62 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Lobster here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lobster extends Actor
+{
+    /**
+     * Act - do whatever the Lobster wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        turnAtEdge();
+        move();
+        eatCrab();
+        randomTurn();
+    }
+    /*
+     * Turns the lobster 17 degrees to the right if it is
+     * touching the edge.
+     */
+    public void turnAtEdge()
+    {
+        if (isAtEdge ())
+        {
+            turn(17);   
+        }   
+    }
+    /*
+     * Moves the lobster 5 constantly.
+     */
+    public void move()
+    {
+         move(5);
+    }
+    /*
+     *Check whether lobster is touching a crab. If it is, eat the crab.
+     *If not, do nothing. 
+     */
+    public void eatCrab()
+    {
+        if(isTouching(Crab.class))
+        {
+            removeTouching(Crab.class);
+        }
+    }
+    /*
+     * Turns the lobster randomly left or right up to 45 degrees 10% of 
+     * the time.
+     */
+    public void randomTurn()
+    {
+        if(Greenfoot.getRandomNumber(100)<10)
+        {
+            turn(Greenfoot.getRandomNumber(91)-45);
+        }
+    } 
+}
