27c716f299be710ee49851551b940b26694d552f
Ray-b25 <raymond.brandeberry@stu.boone.kyschools.us>
2021-03-23 08:39:22 -0400

    Added score counter and win functions

--- Crab.java
+++ Crab.java
@@ -7,14 +7,17 @@ public class Crab extends Actor
 {
     private GreenfootImage image1;
     private GreenfootImage image2;
+    private  int  wormsEaten; 
     public Crab()
     {
     image1 = new GreenfootImage("crab.png"); 
     image2 = new GreenfootImage("crab2.png");
     setImage(image1);
+    wormsEaten  =  0;
     }
     public void act()
     {
+        crabAnimation();
         move();
         eatWorm(); 
         arrowKeyTurn();
@@ -37,6 +40,15 @@ public class Crab extends Actor
         {
             removeTouching(Worm.class);
             Greenfoot.playSound("slurp.wav");
+            wormsEaten  =  wormsEaten  +  1;
+            
+            getWorld().showText("Score: " + wormsEaten, 50, 500);
+            
+            if(wormsEaten  == 10)
+            {
+                Greenfoot.playSound("fanfare.wav");
+                Greenfoot.stop();
+            }
         }
     }
     /*
@@ -52,6 +64,20 @@ public class Crab extends Actor
         if (Greenfoot.isKeyDown("right"))
         {
             turn (5);
+        }
+    }
+    /*
+     * Change the crab's amimation to the other image constantly
+     */
+    public void crabAnimation()
+    {
+         if(getImage() == image1)
+        {
+            setImage(image2);
+        }
+        else
+        {
+            setImage(image1);
         }
     }
 }
